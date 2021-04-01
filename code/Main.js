00a1f6f0b7c247eba9d2ece8736c6884863f0987
Austin Dupras <3ustin@gmail.com>
2021-03-31 23:35:30 -0400

    JsBox&Owl

--- code/Main.js
+++ code/Main.js
@@ -1,9 +1,23 @@
-function makeCircle(element){
-    var ctx = element.getContext("2d");
-    
-    ctx.beginPath();
-    ctx.lineWidth = "6";
-    ctx.strokeStyle = "red";
-    ctx.rect(5, 5, 290, 140);
-    ctx.stroke();
+
+var ctx = document.getElementById("canvas").getContext("2d");
+var image = document.createElement("IMG");
+image.src = "img/GiantOwl.png";
+
+
+ctx.beginPath();
+ctx.lineWidth = "6";
+ctx.strokeStyle = "red";
+ctx.rect(5, 5, 500, 500);
+ctx.stroke();
+
+
+console.log("STOP WATCHING ME");
+
+function makeCircle(){
+    ctx.drawImage(image,80,80,100,100);
+
+}
+
+document.onkeydown = function(e){
+
 }
